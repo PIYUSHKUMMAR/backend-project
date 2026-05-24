73ab1769f8770d22cbfc880f0571c8cbff9cbd7c
PIYUSHKUMMAR <masterpiyushsh@gmail.com>
2026-05-24 23:19:12 +0530

    fix: updated prisma config paths and package dependency

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,9 +2,9 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "../prisma/schema.prisma", // Added ../ to point to the root folder
   migrations: {
-    path: "prisma/migrations",
+    path: "../prisma/migrations",    // Added ../ to point to the root folder
   },
   datasource: {
     url: env("DATABASE_URL"),
